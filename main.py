4a0a5bbeb62d310e18c2ba83a74d069361183583
mistrain <mahdimousavi40@gmail.com>
2023-08-22 12:26:34 +0330

    use Pydantic and create a requset body (swagger)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 # Create an instance of the FastAPI as imported above
 # uvicorn main:app 'main as the filename and app as the FastAPI instance'
@@ -8,6 +8,7 @@ app = FastAPI()
 
 # operation on the path like Get and the route
 # the function is path operation function
+
 
 @app.get('/')
 def index():
@@ -18,18 +19,21 @@ def index():
     }
 
 # set query params like /blog?limit=53
+
+
 @app.get('/blog')
 def blog(limit=10, published: bool = True, sort: Optional[str] = None):
     # only get 10 published blogs
     if published:
-        
-      return {
-          "data": f"blogs: {limit} and published: {published}"
-      }
+
+        return {
+            "data": f"blogs: {limit} and published: {published}"
+        }
     else:
         return {
-          "data": f"blogs: {limit}"
-      } 
+            "data": f"blogs: {limit}"
+        }
+
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -46,3 +50,20 @@ def show(id: int):
             id: id
         }
     }
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {
+        "data": {
+            "name": 'Blog id is ' + f'{id}',
+            id: id
+        }
+    }
+
+class Blog(BaseModel):
+    pass
+
+@app.post('/blog')
+def create_blog(requset: Blog):
+    return {'data': 'blog is created'}
