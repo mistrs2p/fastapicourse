3923535973c46ec26f57eff218fa1a097ea0eac7
mistrain <mahdimousavi40@gmail.com>
2023-08-22 15:46:41 +0330

    add db fn to the path and create set

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from . import schemas, models
-from .database import engin 
-
+from .database import SessionLocal, engin 
+from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engin)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')  
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
