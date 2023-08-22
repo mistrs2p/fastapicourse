df82058ce816c8d537085afecdb1e99af63d9d0e
mistrain <mahdimousavi40@gmail.com>
2023-08-22 13:28:00 +0330

    seperate file and add schemas

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
+from . import schemas
 app = FastAPI()
 
-class Blog(BaseModel):
-    title: str
-    body: str
 
-@app.post('/blog')
-def create(request: Blog):
+@app.post('/blog')  
+def create(request: schemas.Blog):
     return request
