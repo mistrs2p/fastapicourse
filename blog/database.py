ee07d1815a2e1ca320c152b9e6d60bc3ca902b29
mistrain <mahdimousavi40@gmail.com>
2023-08-22 14:45:45 +0330

    some reform set and con to db set with model added

--- blog/database.py
+++ blog/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
-# from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
-# create database engine 
-engin = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# create database engine
+engin = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                      "check_same_thread": False})
 
 
 # create session
