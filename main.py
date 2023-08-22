810a202ebceb8f5db58b15989cd0c28871a89239
mistrain <mahdimousavi40@gmail.com>
2023-08-22 12:04:24 +0330

    add some route and check the order route

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
 
+
+# Create an instance of the FastAPI as imported above
+# uvicorn main:app 'main as the filename and app as the FastAPI instance'
 app = FastAPI()
+
+# operation on the path like Get and the route
+# the function is path operation function
+
 
 @app.get('/')
 def index():
-    return 'heyy'
+    return {
+        "data": {
+            "name": 'mahdi'
+        }
+    }
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {
+        "data": "unpublished"
+    }
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {
+        "data": {
+            "name": 'Blog id is ' + f'{id}',
+            id: id
+        }
+    }
