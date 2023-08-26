dffe6e0a64283bf18300de61458c7f64ede6b62b
mistrain <mahdimousavi40@gmail.com>
2023-08-26 15:32:01 +0330

    add auth login rep and fn

---
+++ blog/repository/authentication.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+from fastapi import Depends, status, HTTPException
+from .. import models, database, schemas
+from ..hashing import Hash
+
+
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email==request.username ).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+    return user
