f71d6e5dbf1cf895060f870ea4f5a11a9a319511
mistrain <mahdimousavi40@gmail.com>
2023-08-26 17:58:34 +0330

    add oauth to all route

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -9,26 +9,26 @@ router = APIRouter(prefix='/blog', tags=['Blogs'])
 
 
 @router.get('', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
-def all(db: Session = Depends(database.get_db), get_current_user: schemas.User = Depends(oauth2.get_current_user)):
+def all(db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
 @router.post('', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
+def create(request: schemas.Blog, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
     
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete(id: int, db: Session = Depends(database.get_db)):
+def delete(id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.destroy(id, db)
 
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(request: schemas.Blog, id: int, db: Session = Depends(database.get_db)):
+def update(request: schemas.Blog, id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(request, id, db)
 
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def show(id: int, db: Session = Depends(database.get_db)):
+def show(id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id, db)
 
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,17 +1,19 @@
 from fastapi import APIRouter, Depends
-from .. import schemas, database
+from .. import schemas, database, oauth2
 # from typing import List
 from ..hashing import Hash
 from sqlalchemy.orm import Session
 from ..repository import user
+
+
 router = APIRouter(prefix='/user', tags=['Users'])
 
 
 @router.post('')
-def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
+def create_user(request: schemas.User, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return user.create(request, db)
 
 
 @router.get('/{id}', response_model=schemas.ShowUser)
-def show_user(id: int, db: Session = Depends(database.get_db)):
+def show_user(id: int, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return user.show(id, db)
