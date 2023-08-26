81e5c6abf5aa6c1bc9e46f0a1ca95fe7ef75759f
mistrain <mahdimousavi40@gmail.com>
2023-08-26 15:32:16 +0330

    add login path and set

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from .. import schemas, models, database
+from sqlalchemy.orm import Session
+from ..repository import authentication 
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login():
-    return 'login'
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+    return authentication.login(request, db)
