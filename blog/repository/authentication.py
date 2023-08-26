eb9fa2602a1ed460b5e50607801c9a32fe886c1d
mistrain <mahdimousavi40@gmail.com>
2023-08-26 15:42:30 +0330

    add login fn route fn for hash verify

--- blog/repository/authentication.py
+++ blog/repository/authentication.py
@@ -8,4 +8,7 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email==request.username ).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f'Incorrect Password')
+    # JWT token
     return user
