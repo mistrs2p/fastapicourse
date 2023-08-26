a908376f703c31435b2481a65600d2a901c27975
mistrain <mahdimousavi40@gmail.com>
2023-08-26 17:47:58 +0330

    add proper schema that depend on nothing

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, models, database
 from sqlalchemy.orm import Session
 from ..repository import authentication 
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     return authentication.login(request, db)
