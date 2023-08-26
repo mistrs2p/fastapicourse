a08b15b77d7019dd6a1c5499710de3c453f0c4af
mistrain <mahdimousavi40@gmail.com>
2023-08-26 11:33:44 +0330

    add tags to group same endpoint route

--- blog/main.py
+++ blog/main.py
@@ -19,7 +19,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 # db is session that depends on database
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -29,7 +29,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def delete(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
 
@@ -41,7 +41,7 @@ def delete(id: int, db: Session = Depends(get_db)):
     return {"detail": f"Blog with id of {id} is removed!"}
 
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(request: schemas.Blog, id: int, db: Session = Depends(get_db)):
     # db.query(models.Blog).filter(models.Blog.id == id).update({
     #     models.Blog.title: request.title,
@@ -56,14 +56,14 @@ def update(request: schemas.Blog, id: int, db: Session = Depends(get_db)):
     return {"detail": f"Blog with id of {id} is updated!"}
 
 
-@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
+@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog], tags=['blogs'])
 def all(db: Session = Depends(get_db)):
     # quereing on model
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog, tags=['blogs'])
 def show(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -76,7 +76,7 @@ def show(id: int, db: Session = Depends(get_db)):
 
 
 
-@app.post('/user')
+@app.post('/user', tags=['users'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(
         name= request.name,
@@ -89,7 +89,7 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.get('/user/{id}', response_model=schemas.ShowUser)
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
 def show_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
