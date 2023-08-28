81355588d24cfe33fe6b92b6dc2f4db1f952b807
mistrain <mahdimousavi40@gmail.com>
2023-08-28 14:34:56 +0330

    add origin and middleware config

--- blog/main.py
+++ blog/main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI
 from . import models
 from .database import engin
 from .routers import blog, user, authentication
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 models.Base.metadata.create_all(bind=engin)
 app.include_router(blog.router)
 app.include_router(user.router)
