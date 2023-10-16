2868466ea485f58b5ab716958915e67ce2a08ae7
mistrain <mahdimousavi40@gmail.com>
2023-10-16 19:27:45 +0330

    bug fixed

--- blog/token.py
+++ blog/token.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from . import schemas
+from typing import Union
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -24,6 +25,6 @@ def verify_token(token: str, credentials_exception):
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
-        token_data = schemas.TokenData(email=email)
+        sadfasfd = schemas.TokenData(email=email)
     except JWTError:
         raise credentials_exception
